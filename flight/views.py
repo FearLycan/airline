756a3258c7c909f1782bab554740d9d37bdf1e45
Damian Brończyk <damian.bronczyk@tauceti.email>
2018-07-06 12:12:45 +0200

    add passengers to flight

--- flight/views.py
+++ flight/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from flight.models import Flight, Airport
+from django.urls import reverse
+
+from flight.models import Flight, Airport, Passenger
 
 
 def index(request):
@@ -20,4 +22,21 @@ def view(request, flight_id):
     return render(request, 'flight/view.html', {
         'flight': model,
         'passengers': model.passengers.all(),
+        'non_passengers': Passenger.objects.exclude(flights=model).all()
     })
+
+
+def book(request, flight_id):
+    try:
+        passenger_id = int(request.POST['passenger'])  # check if passenger was POST
+        passenger = Passenger.objects.get(pk=passenger_id)  # check if passenger exist
+        flight = Flight.objects.get(pk=flight_id)  # check if flight exist
+    except KeyError:
+        return render(request, 'flight/error.html', {'message': 'No selection'})
+    except Passenger.DoesNotExist:
+        return render(request, 'flight/error.html', {'message': 'No passenger'})
+    except Flight.DoesNotExist:
+        return render(request, 'flight/error.html', {'message': 'No flight'})
+
+    passenger.flights.add(flight)
+    return HttpResponseRedirect(reverse('view', args=(flight_id,)))
